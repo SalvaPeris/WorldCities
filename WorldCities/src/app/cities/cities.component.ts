df0c50a8ebe71a7b2064afa20dacf4de53112fa3
Salva Peris Gimeno <sperisgimeno@gmail.com>
2022-09-20 17:31:41 +0200

    20.09 - Pág. 233.

--- WorldCities/src/app/cities/cities.component.ts
+++ WorldCities/src/app/cities/cities.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 import { City } from './city';
 
@@ -12,14 +14,17 @@ import { City } from './city';
 
 export class CitiesComponent implements OnInit {
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
-  public cities!: City[];
+  public cities!: MatTableDataSource<City>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get<City[]>(environment.baseUrl + 'api/Cities')
       .subscribe(result => {
-        this.cities = result;
+        this.cities = new MatTableDataSource<City>(result);
+        this.cities.paginator = this.paginator;
       }, error => console.error(error));
   }
 }
